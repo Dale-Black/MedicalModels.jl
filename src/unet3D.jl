ccd74b875d9a1725bf2f0c54400edefb1b5e2e67
Dale <djblack@uci.edu>
2021-02-19 16:15:15 -0800

    update

--- src/unet3D.jl
+++ src/unet3D.jl
@@ -10,6 +10,6 @@ function unet3D(in_chs, lbl_chs)
     l6 = Chain(l5, tran2(128, 64), conv1(64, 64))
     l7 = Chain(Parallel(+, l6, l4), tran2(64, 32), conv1(32, 32))       # Residual connection between l6 & l4
     l8 = Chain(Parallel(+, l7, l3), tran2(32, 16), conv1(16, 16))       # Residual connection between l7 & l3
-    l9 = Chain(Parallel(+, l8, l2), tran2(16, 4), conv1(4, 4), )        # Residual connection between l8 & l2
+    l9 = Chain(Parallel(+, l8, l2), tran2(16, 4), conv1(4, 4))        # Residual connection between l8 & l2
     l10 = Chain(l9, conv1(4, lbl_chs))
 end
